2de4b07f492a970a9069d1135c4492ec07a143a1
aleksander <s.sith@yandex.r>
2023-01-20 21:49:22 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict"
+
+let number = 4.6;
+
+const answer = [];
+
+answer[0] = prompt("Как ваше имя?", "");
+answer[1] = prompt("Как ваша фамилия?", "");
+answer[2] = +prompt("Сколько вам лет?", "");
+
+
+
+document.write(answer);
